9b018c2a2357603561940d59438f5ccbfa743c28
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-10-11 19:05:29 +0000

    add css

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React from "react"
+import css from "./css/Sidebar.module.css"
 
 function FunctionalSidebar() {
-    return <div><a>My Photos</a><a>My Illustrations</a><a>My Paintings</a></div>
+    return <div className={css.sidebar}><a>My Photos</a><a>My Illustrations</a><a>My Paintings</a></div>
 }
 
 export default FunctionalSidebar
